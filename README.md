32ef0649f66b1a7fde5e2a96525c09c3c73b7681
Adishree Das <dasadishree@gmail.com>
2025-12-30 17:20:23 -0500

    basic basic version

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# AI solution to food waste by predicting how many people will buy what types of school lunches
+# Presidential AI Challenge!
+Making an AI solution helping people (particularly teenagers) understand what they are consuming by looking at ingredient labels!
+
+You've likely heard of the saying that you shouldn't eat something if you can't pronounce the ingredients, but my program tries to actually teach people about what they are putting into their bodies. I was inspired about my unhealthy energy drink addiction for this project - at first, I used to think that energy drinks were simply just a better tasting alternative to coffee with only caffeine in it. However, I learned about other added ingredients including legal stimulants like guarana, taurine, and L-carnitine which can be unhealthy, harming the heart and nervous system. 
---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+client=OpenAI(api_key=os.getenv("API_KEY"))
+
+def explainIngredients(ingredientList):
+    prompt=f"""
+    Explain each ingredient below. 
+    For each ingredient, say:
+    - What it is (both scientifically and common names)
+    - Why it's used
+    - Possible effects, both positive and negative
+    Make this understandable to the average person so that they can make informed decisions and actually understand what they are eating/consuming.set
+
+    Ingredients:
+    {ingredientList}
+    """
+
+    response= client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You're an assistant/food nutrition scientist helping the average person understand ingredient lists for their food items"},
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    return response.choices[0].message.content
