2fbf6eff8b5cf7f2d091c0dfefa8b585c7861aaf
Adishree Das <dasadishree@gmail.com>
2025-12-30 19:05:54 -0500

    hackclub api

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
-from openai import OpenAI
+import requests
+# from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
-client=OpenAI(api_key=os.getenv("API_KEY"))
+API_KEY = os.getenv("API_KEY")
+url="https://api.openai.com/v1/chat/completions"
 
 def explainIngredients(ingredientList):
     prompt=f"""
@@ -12,21 +14,28 @@ def explainIngredients(ingredientList):
     - What it is (both scientifically and common names)
     - Why it's used
     - Possible effects, both positive and negative
-    Make this understandable to the average person so that they can make informed decisions and actually understand what they are eating/consuming.set
+    Make this understandable to the average person so that they can make informed decisions and actually understand what they are eating/consuming
 
     Ingredients:
-    {ingredientList}
+
     """
 
-    response= client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
+    data ={
+        "model": "qwen/qwen3-32b",
+        "messages": [
             {"role": "system", "content": "You're an assistant/food nutrition scientist helping the average person understand ingredient lists for their food items"},
-            {"role": "user", "content": prompt}
+            {"role": "user", "content" : prompt}
         ]
-    )
+    }
 
-    return response.choices[0].message.content
+    headers={
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    response.raise_for_status()
+    return response.json()["choices"][0]["message"]["content"]
 
 #input
 ingredients = input("Paste ingredient list here:")
